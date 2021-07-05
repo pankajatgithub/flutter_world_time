999e1ba44bd0f5674f255b67d18b5643847053ee
Pankaj <pankaj200635@gmail.com>
2021-07-06 02:53:50 +0530

    Route and Map for world time app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
-import 'package:flutter/material.dart';
 
-import 'Pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:world_time/Pages/choose_location.dart';
+import 'package:world_time/Pages/loading.dart';
+import 'package:world_time/Pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:Home(),
+    // home:Home(),  it will give error as default route is defined as loading page i.e. "/"
+  initialRoute: "/home",//not necessary,we can set initial route if we want default page
+                       // other than default route
+
+    routes: {
+      "/":(context)=>Loading(),
+      "/home":(context)=>Home(),
+      "/location":(context)=>ChooseLocation()
+    },
   ));
 }
 
