57fb413287d7f87c8e01e7f5e5d923d18187a764
Pankaj <pankaj200635@gmail.com>
2021-07-15 03:11:35 +0530

    Formatting date in flutter

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,6 +1,8 @@
 
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
+
 
  class WorldTime{
 
@@ -28,7 +30,7 @@ import 'dart:convert';
         //   create dateTime Object
         DateTime now=DateTime.parse(dateTime);
         now=now.add(Duration(hours:int.parse(offset)));
-        time=now.toString();
+        time=DateFormat.jm().format(now);
         print(time);
 
       }catch(e){
@@ -38,26 +40,5 @@ import 'dart:convert';
       }
 
     }
-
-    // void getData() async{
-    //   Response response=await get(Uri.parse("http://worldtimeapi.org/api/timezone/Europe/London"));
-    //   print(response.body);
-    //   //import dart:convert to use jsondecode method
-    //   // here we will need to add datetime with offset to get actual local time
-    //   Map data=jsonDecode(response.body);
-    //   String dateTime=data['datetime'];
-    //   //ERROR:flutter/lib/ui/ui_dart_state.cc(199)] Unhandled Exception: FormatException: Invalid radix-10 number (at character 2)
-    //   // E/flutter ( 5677): +01:00 ,if we add directly to datetime without substring
-    //   String offset=data['utc_offset'].substring(1,3);
-    //
-    //   print(dateTime);
-    //   print(offset);
-    //   //   create dateTime Object
-    //   DateTime now=DateTime.parse(dateTime);
-    //   now=now.add(Duration(hours:int.parse(offset)));
-    //   print(now);
-    //
-    // }
-
 
     }
