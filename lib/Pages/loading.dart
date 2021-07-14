30ad5726bd123e2c8056d448c52e1473e58a5373
Pankaj <pankaj200635@gmail.com>
2021-07-15 03:41:07 +0530

    Loader/spinner in flutter

--- lib/Pages/loading.dart
+++ lib/Pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -36,10 +37,15 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(100.0),
-        child: Text("loading"),
-      ),
+      backgroundColor:Colors.blue[900],
+      body: Center(
+
+        child:SpinKitFadingCube( //ex.SpinKitRotatingCircle
+          color: Colors.white,
+          size: 50.0,
+        )
+
+      )
     );
   }
 
